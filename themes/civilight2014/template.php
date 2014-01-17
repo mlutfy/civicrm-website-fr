cc4db70319b8ccb6b450496bfd8b388526f92107
Mathieu Lutfy <mathieu@bidon.ca>
2014-01-17 16:23:34 -0500

    civilight2014: template.php: implement hook_links() to hide blog links.

--- themes/civilight2014/template.php
+++ themes/civilight2014/template.php
@@ -4,3 +4,13 @@
  * @file
  * template.php
  */
+
+/**
+ * Implements hook_links().
+ */
+function civilight2014_links($links, $attributes = array()) {
+  // Hide "foo's blog" in links below blog teasers.
+  // Confusing and serves little purpose.
+  unset($links['blog_usernames_blog']);
+  return theme_links($links, $attributes);
+}
